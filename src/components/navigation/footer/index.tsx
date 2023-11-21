797bc7e3eec20dc738c0d3b2e6e2f5bcf8f7090e
B1RDOW <griner710@gmail.com>
2023-11-22 00:58:32 +0600

    Next.js 14 Fullstack Telegra.ph Clone | Fixed Git Link

--- src/components/navigation/footer/index.tsx
+++ src/components/navigation/footer/index.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
 			<div className="container px-4 mx-auto grid grid-flow-col gap-x-3 justify-center text-center items-center text-zinc-500">
 				<p>Developed by <Link href="https://github.com/B1RDOW" className="underline hover:decoration-wavy decoration-dashed decoration-1 font-medium dark:text-zinc-200 text-zinc-800 transition" target="_blank" rel="noreferrer noopener">BIRDOW</Link></p>
 				<p className="select-none">|</p>
-				<p>Source on <Link href="https://github.com/B1RDOW/bi-graph" className="underline hover:decoration-wavy decoration-dashed decoration-1 font-medium dark:text-zinc-200 text-zinc-800 transition" target="_blank" rel="noreferrer noopener">GitHub</Link></p>
+				<p>Source on <Link href="https://github.com/B1RDOW/BIgraph" className="underline hover:decoration-wavy decoration-dashed decoration-1 font-medium dark:text-zinc-200 text-zinc-800 transition" target="_blank" rel="noreferrer noopener">GitHub</Link></p>
 			</div>
 		</footer>
 	)
